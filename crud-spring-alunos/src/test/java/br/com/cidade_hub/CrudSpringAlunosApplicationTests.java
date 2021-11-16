b7160de5f80051f3f2224365fad7f734b085c33d
Catarino Rodrigues <contabilnc@hotmail.com>
2021-11-16 09:55:36 -0300

    Criação do Projeto

---
+++ crud-spring-alunos/src/test/java/br/com/cidade_hub/CrudSpringAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cidade_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
